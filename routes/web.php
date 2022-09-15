656e5f8dd3a121d3f2f6e22447e6668c764ce0c8
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-09-15 17:31:01 +0300

    First controller MyPlaceController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('my_page', function () {
-    return ('is my page');
-});
+Route::get('my_page', 'MyPlaceController@index');
