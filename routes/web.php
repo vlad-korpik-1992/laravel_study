a7ec1c375dbfbcf9eeb2d65e99001d6a5485c5fd
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-09-16 17:29:30 +0300

    method for reading data from the database

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('my_page', 'MyPlaceController@index');
+Route::get('post', 'PostController@index');
