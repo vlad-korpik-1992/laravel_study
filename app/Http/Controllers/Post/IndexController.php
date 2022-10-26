f33a381b945f3f6192b1236585fb65d94c66ac8f
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-10-26 15:44:47 +0300

    Added pagination

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\Tag;
-use Illuminate\Http\Request;
 
 class IndexController extends BaseController
 {
     public function __invoke()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(10);
         return view('post.index', compact('posts'));
     }
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.main')
 @section('content')
     <div class="container">
-        <div class="row mb-3">
+        <div class="row">
+            <div class="col-12">
+                <a href="{{route('posts.create')}}" class="btn btn-primary">Create</a>
+            </div>
+        </div>
+        <div class="row mt-3">
             @foreach ($posts as $post)
                 <div class="col-12">
                     <a href="{{route('posts.show',$post->id)}}">{{ $post->id }} {{$post->title}}</a>
                 </div>
             @endforeach
         </div>
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-12">
-                <a href="{{route('posts.create')}}" class="btn btn-primary">Create</a>
+                {{ $posts->links() }}
             </div>
         </div>
     </div>
