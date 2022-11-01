41eff11cd00f8af15a56d8110446a7ad54a868d3
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-11-01 16:26:54 +0300

    Class http Guzzle

---
+++ app/Console/Commands/ImportJsonPlaceholderCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use Illuminate\Console\Command;
+use App\Models\Post;
+
+class ImportJsonPlaceholderCommand extends Command
+{
+    protected $signature = 'import:jsonplaceholder';
+
+    protected $description = 'Get data from jsonplaseholder';
+
+    public function handle()
+    {
+        $import = new ImportDataClient();
+        $response = $import->client->request('GET', 'posts');
+        $data = json_decode($response->getBody()->getContents());
+
+        foreach ($data as $item) {
+            Post::firstOrCreate([
+                'title' =>  $item->title
+            ],[
+                'title' => $item->title,
+                'content' => $item->body,
+                'category_id' => 2,
+            ]);
+        }
+    }
+}
